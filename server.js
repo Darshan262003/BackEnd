3fe5bc5bd429d2d2e8bdb05954339e438aa97174
darshan <darudarshan2207@gmail.com>
2026-02-27 17:11:20 +0530

    Update CORS configuration and frontend URL settings

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 // Validation helper
